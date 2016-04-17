463abd6830b33e36d6d1fddf3a76a47e61c0754d
Stylianos Modes <stelmod@gmail.com>
2016-04-17 23:16:30 +0100

    Added RMSE evalutation and loaded movielens data

--- src/main/java/com/stelmod/recommendations/App.java
+++ src/main/java/com/stelmod/recommendations/App.java
@@ -1,31 +1,34 @@
 package com.stelmod.recommendations;
 
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import java.io.File;
 import java.net.URL;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        URL inputStream = Thread.currentThread().getContextClassLoader().getResource("sample.csv");
+        URL inputStream = Thread.currentThread().getContextClassLoader().getResource("ml-small.csv");
 
         File inputFile = new File(inputStream.toURI());
         DataModel dataModel = new FileDataModel(inputFile);
-        UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
-        UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel);
-        Recommender recommender = new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
-        List<RecommendedItem> recommendedItems = recommender.recommend(1, 1);
-        for (RecommendedItem recommendedItem : recommendedItems) {
-            System.out.println(recommendedItem);
-        }
+
+        RecommenderEvaluator evaluator = new RMSRecommenderEvaluator();
+        RecommenderBuilder recommenderBuilder = dataModel1 -> {
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel1);
+            UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel1);
+            return new GenericUserBasedRecommender(dataModel1, neighborhood, similarity);
+        };
+
+        double score = evaluator.evaluate(recommenderBuilder, null, dataModel, 0.7, 1.0);
+        System.out.println(score);
     }
 }
