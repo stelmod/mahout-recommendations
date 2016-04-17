96c738cb6081888cde152186e08a7d697f0387d4
Stylianos Modes <stelmod@gmail.com>
2016-04-18 00:35:42 +0100

    Ensure similarity calculation is performed across the whole dataset

--- src/main/java/com/stelmod/recommendations/NN_Evaluator.java
+++ src/main/java/com/stelmod/recommendations/NN_Evaluator.java
@@ -4,7 +4,9 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class NN_Evaluator {
     private final DataModel dataModel;
@@ -16,13 +18,15 @@ public class NN_Evaluator {
     }
 
     public void evaluateModel() throws Exception {
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+
         for (int neighbourhoodSize : neighbourhoodSizes) {
-            trainAndEvaluareError(dataModel, new RMSRecommenderEvaluator(), neighbourhoodSize);
+            trainAndEvaluareError(dataModel, new RMSRecommenderEvaluator(), neighbourhoodSize, similarity);
         }
     }
 
-    private double trainAndEvaluareError(DataModel dataModel, RecommenderEvaluator evaluator, int neighbourhoodSize) throws TasteException {
-        RecommenderBuilder recommenderBuilder = new NN_Pearson_GenericRecommender(neighbourhoodSize);
+    private double trainAndEvaluareError(DataModel dataModel, RecommenderEvaluator evaluator, int neighbourhoodSize, UserSimilarity similarity) throws TasteException {
+        RecommenderBuilder recommenderBuilder = new NN_Pearson_GenericRecommender(neighbourhoodSize, similarity);
         double score = evaluator.evaluate(recommenderBuilder, null, dataModel, 0.8, 1.0);
         System.out.println("Neighbourhood size: " + neighbourhoodSize + ", RMSE: " + score);
         return score;
--- src/main/java/com/stelmod/recommendations/NN_Pearson_GenericRecommender.java
+++ src/main/java/com/stelmod/recommendations/NN_Pearson_GenericRecommender.java
@@ -5,7 +5,6 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
@@ -13,14 +12,15 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class NN_Pearson_GenericRecommender implements RecommenderBuilder {
     private final int neighbourhoodSize;
+    private final UserSimilarity similarity;
 
-    public NN_Pearson_GenericRecommender(int neighbourhoodSize) {
+    public NN_Pearson_GenericRecommender(int neighbourhoodSize, UserSimilarity similarity) {
         this.neighbourhoodSize = neighbourhoodSize;
+        this.similarity = similarity;
     }
 
     @Override
     public Recommender buildRecommender(DataModel dataModel) throws TasteException {
-        UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
         UserNeighborhood neighborhood = new NearestNUserNeighborhood(neighbourhoodSize, similarity, dataModel);
         return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
     }
