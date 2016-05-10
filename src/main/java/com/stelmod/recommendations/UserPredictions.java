951178c3afc0f0e35f41cef5add904f16700d792
Stelios Modes <smodes@thoughtworks.com>
2016-05-10 23:11:43 +0100

    Output movie titles instead of just ids

--- src/main/java/com/stelmod/recommendations/UserPredictions.java
+++ src/main/java/com/stelmod/recommendations/UserPredictions.java
@@ -1,5 +1,7 @@
 package com.stelmod.recommendations;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
@@ -9,8 +11,11 @@ import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserPredictions {
 
@@ -19,8 +24,9 @@ public class UserPredictions {
     public static final int NEIGHBORHOOD_SIZE = 20;
 
     public static void main(String[] args) throws Exception {
-        URL inputStream = Thread.currentThread().getContextClassLoader().getResource("ml-small.csv");
+        Map<Long, String> movies = loadMoviesData();
 
+        URL inputStream = Thread.currentThread().getContextClassLoader().getResource("ml-small.csv");
         File inputFile = new File(inputStream.toURI());
         DataModel dataModel = new FileDataModel(inputFile);
 
@@ -29,6 +35,18 @@ public class UserPredictions {
         Recommender recommender = recommenderBuilder.buildRecommender(dataModel);
 
         List<RecommendedItem> recommendedItems = recommender.recommend(USER_ID, NUMBER_OF_RECOMMENDATIONS);
-        recommendedItems.forEach(item -> System.out.println(item));
+        recommendedItems.forEach(item -> System.out.println(movies.get(item.getItemID()) + " with rating " + item.getValue()));
+    }
+
+    private static HashMap<Long, String> loadMoviesData() throws Exception {
+        HashMap<Long, String> movies = new HashMap<>();
+        URL inputStream = Thread.currentThread().getContextClassLoader().getResource("movies.csv");
+        File csvData = new File(inputStream.toURI());
+        CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvData)).withSkipLines(1).build();
+        String[] csvRecord;
+        while ((csvRecord = csvReader.readNext()) != null) {
+            movies.put(Long.parseLong(csvRecord[0]), csvRecord[1]);
+        }
+        return movies;
     }
 }
